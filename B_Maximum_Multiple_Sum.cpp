b308b15932460ed1224ffb615240e46810e280f7
Khondaker Faisal Ibn Aziz <111153120+FaisalMahi@users.noreply.github.com>
2024-06-11 21:46:23 +0600

    Add files via upload

---
+++ B_Maximum_Multiple_Sum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test; cin>>test;
+    while(test--)
+    {
+        int n; cin>>n;
+        if(n<=3) cout<<n<<endl;
+        else cout<<2<<endl;
+    }
+    return 0;
+}
