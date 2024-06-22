bdf130f6582dd8921748d1521c7b9cc0e60f55f2
Khondaker Faisal Ibn Aziz <111153120+FaisalMahi@users.noreply.github.com>
2024-06-22 18:43:45 +0600

    Add files via upload

--- C_Clock_and_Strings.cpp
+++ C_Clock_and_Strings.cpp
@@ -5,9 +5,22 @@ int main()
     int test; cin>>test;
     while(test--)
     {
-    int a,b,c,d;
-    cin>>a>>b>>c>>d;
-    if(c>a&&c<b||c<a&&c>b||d>a&&d<b||d<a&&d>b) cout<<"YES"<<endl;
+            int a,b,c,d; cin>>a>>b>>c>>d;
+    int flag1=0,flag2=0;
+    int maximum= max(a,b);
+    int minimum=min(a,b);
+    for(int i =minimum;i<=maximum;i++)
+    {
+        if(i==c)
+        {
+            flag1=1;
+        }
+        if(i==d)
+        {
+            flag2=1;
+        }
+    }
+    if(flag1!=flag2) cout<<"YES"<<endl;
     else cout<<"NO"<<endl;
     }
     return 0;
