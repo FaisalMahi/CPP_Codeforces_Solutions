9408ec7b6be33d3ceee21d869e3d62fd229445d9
Khondaker Faisal Ibn Aziz <111153120+FaisalMahi@users.noreply.github.com>
2024-06-23 21:22:01 +0600

    Add files via upload

---
+++ A_X_Axis.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int ar[3];
+    for(int i =0;i<3;i++)
+    {
+        cin>>ar[i];
+    }
+    sort(ar,(ar+3));
+    int ans1,ans2,ans3;
+    ans1=ar[ans2-ans1];
+    ans1=ar[ans3-ans2];
+    ans1=ar[ans3-ans1];
+    int cmp1,cmp2;
+    if(ans1<=ans2&&ans1<=)
+    return 0;
+}
