c00e72a7cfb85f0a777999251880e25c14782419
Khondaker Faisal Ibn Aziz <111153120+FaisalMahi@users.noreply.github.com>
2024-07-01 23:24:41 +0600

    Add files via upload

---
+++ A_Upload_More_RAM.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test; cin>>test;
+    while(test--)
+    {
+    int a,b;
+    cin>>a>>b;
+    int ans=(1+(a-1)*b);
+    cout<<ans<<endl;
+    }
+    return 0;
+    
+}
