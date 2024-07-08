eb2ea3dc328db8cb5cfe6070cc69fd35a75fac62
Khondaker Faisal Ibn Aziz <111153120+FaisalMahi@users.noreply.github.com>
2024-07-09 02:55:14 +0600

    Add files via upload

--- A_Don_t_Try_to_Count.cpp
+++ A_Don_t_Try_to_Count.cpp
@@ -1,25 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int test; cin>>test;
-    while(test--)
+    int t;
+    cin >> t;
+    while (t--)
     {
-    int n1,n2,count=0; cin>>n1>>n2;
-    string s1, s2; cin>>s1>>s2;
-    if(n1!=n2&&n1*2>n2)
-    {
-        count=-1;
-    }
-    else
-    {
-        while(n1<=n2)
+        int n1, n2;
+        cin >> n1 >> n2;
+        string s1, s2;
+        cin >> s1 >> s2;
+        int result = 0, test = 6, flag = 0;
+        while (test--)
         {
-            count++;
-            n1=n1*2;
+            if (s1.find(s2) != string ::npos)
+            {
+                flag = 1;
+                break;
+            }
+            result++;
+            s1 += s1;
         }
-    }
-    cout<<count<<endl;
+        if (flag == 1)
+            cout << result << endl;
+        else
+            cout << -1 << endl;
     }
     return 0;
 }
